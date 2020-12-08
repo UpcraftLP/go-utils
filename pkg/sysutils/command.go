2578aca67b4155e0b9bb161ead72fa42d4e3ae7d
UpcraftLP <j-schirra@gmx.de>
2020-12-08 17:51:34 +0100

    add sysutils::Start

---
+++ pkg/sysutils/command.go
@@ -0,0 +1,19 @@
+package sysutils
+
+import (
+	"os"
+	"os/exec"
+)
+
+func Start(args ...string) (p *os.Process, err error) {
+	if args[0], err = exec.LookPath(args[0]); err == nil {
+		var procAttr os.ProcAttr
+		procAttr.Files = []*os.File{os.Stdin,
+			os.Stdout, os.Stderr}
+		p, err := os.StartProcess(args[0], args, &procAttr)
+		if err == nil {
+			return p, nil
+		}
+	}
+	return nil, err
+}
